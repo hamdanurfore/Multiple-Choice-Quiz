fe81f490e4868f00a20e38602b8dacb36c816d81
Hamda <nurfore14@gmail.com>
2023-11-17 01:43:09 +0000

    start screen ends and question screen shown.

--- logic.js
+++ logic.js
@@ -1,10 +1,15 @@
-// timer, current question and score are initialised
-var timer = 60;
-var currentQuestion = 0;
+// time, question number and score are initialised
+var time = 60;
+var questionNumber = 0;
 var score = 0;
 
 // event of clicking the button is created using id "start"
 document.getElementById("start").addEventListener("click", startQuiz);
 
 // timer id
-var  timerId;
+var  timeInterval;
+
+// when start quiz clicked: start screen ends and question screen shown.
+var startScreen = document.getElementById("start-screen");
+var endScreen = document.getElementById("end-screen");
+var questions = document.getElementById("questions");
