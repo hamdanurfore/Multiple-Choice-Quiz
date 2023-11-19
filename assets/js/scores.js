553f31d76d9358e77ed8ad3eb8db014fb8490ffa
Hamda <nurfore14@gmail.com>
2023-11-19 22:37:17 +0000

    displaying highscores

---
+++ assets/js/scores.js
@@ -0,0 +1,33 @@
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+    // sort highscores by score property in descending order
+    highscores.sort(function (a, b) {
+        return b.score - a.score;
+
+    });
+
+    // Have a for loop that will loop through all the high scores
+    for (var i = 0; i < highscores.length; i += 1) {
+        // create li tag for each high score
+        var listItem = document.createElement('li');
+        listItem.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+
+        // display on page
+        var olEl = document.getElementById('highscores');
+        olEl.appendChild(liTag);
+
+    }
+}
+
+// Then have a function that will Clear High Scores from your local storage and reload your browser window
+function clearHighscores() {
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+}
+
+document.getElementById('clear').onclick = clearHighscores;
+
+// run function when page loads
+printHighscores();
